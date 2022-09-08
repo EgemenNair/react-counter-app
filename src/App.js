58da201c15886b330195fa3d6640c2e6e3dc7995
EgemenNair <nairegemen@gmail.com>
2022-09-08 21:35:29 +0300

    Master2 | Counter() component imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Counter from "./components/Counter.js";
 function App() {
   return (
     <div className="App">
-      
+      <Counter/>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Counter() {
+    return (
+        "Counter"
+    );
+}
+
+export default Counter;
